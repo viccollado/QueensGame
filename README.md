dfff93c6b8d1551d6821d7fc023fc3d05cd5a026
Colladito <victorcollado1@gmail.com>
2025-06-05 13:59:38 +0200

    Add Readme and update main function

---
+++ README.md
@@ -0,0 +1,20 @@
+# ♛ LinkedIn Queens Solver Bot
+
+Welcome to the LinkedIn Queens Solver Bot repository!
+
+This project showcases a bot that solves the Queens Game using advanced optimization techniques. The game solution is computed using Mixed-Integer Linear Programming (MILP), a powerful method for tackling combinatorial challenges like the classic N-Queens problem.
+
+## 🚀 Features
+
+- ✅ MILP-based solver for the Queens Game
+- 🧠 Intelligent and efficient optimization strategy
+- 🖥️ Automated gameplay through a bot interface using `Pyautogui`.
+- 🎥 Demo video of the solver in action
+
+## 📹 Demo
+
+Watch the bot in action solving the Queens Game:
+
+<video width="1280" height="720" controls>
+  <source src="imgs/Queens_03062025.mp4" type="video/mp4">
+</video>
--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-import pyautogui
-import numpy as np
-from queens_solver import queens_solver
-import time
 import datetime
+import time
+
+import numpy as np
+import pyautogui
+
+from queens_solver import queens_solver
 
 
 def main():
+    """
+    Main function to run the queens game solver
 
+    In this function you should change the Queens linkedin game box coordinates. 
+    You can check what coordinates are appropiated in you screen by running the script mouse_info.py.
+    """
     # Wait for 3 seconds to switch to the game window
     time.sleep(3)
 
-    # Start button
+    # Queens start button
     x_start = 1332
     y_start = 814
     # Click on the start button
@@ -19,20 +26,25 @@ def main():
     # Wait for the game to load
     time.sleep(0.5)
 
-    # Parameters of the board
-    top_left_x = 708 # 708  Final 1192
-    top_left_y = 342    # 342 Final 827
-    bottom_right_x = 1192 # 1194 Final 1687
-    # bottom_right_y = 827 # 829 Final 1322
-    box_width = bottom_right_x - top_left_x  # Width of the box in pixels
+    # Queens box parameter
+    # CHANGE BY YOUR COORDINATES
+    top_left_x = 708    # Top left x coordinate
+    top_left_y = 342    # Top left y coordinate
+    
+    bottom_right_x = 1192   # Botton right x coordinate
+    # bottom_right_y = 827  # Botton right y coordinate
 
-    # box_width = 485     # Assume the box are squared
+    # Queens box with
+    box_width = bottom_right_x - top_left_x 
+
+    # Screenshot of the Queens box
     date = datetime.datetime.now()
     screenshot = pyautogui.screenshot(f"Data/queens_{date.year}_{date.month}_{date.day}.png")
 
+    # Determine the number of colors (Crowns to use) in the current game
     diff_colors = 1
-    previous_color = (0, 0, 0)
-    y = top_left_y + 15
+    previous_color = (0, 0, 0)  # White color as initial since there are no white color in the game
+    y = top_left_y + 15         # Row of pixels to check the color
 
     for x in range(top_left_x + 10, bottom_right_x - 10):
         current_color = screenshot.getpixel((x, y))
@@ -40,41 +52,38 @@ def main():
             diff_colors += 1
         previous_color = current_color
 
-    n_box = diff_colors // 2
-    print(f"Number of colors: {n_box}")
+    # Number of crowns in the current game
+    n_crown = diff_colors // 2
+    print(f"Number of colors: {n_crown}")
 
-    # n_box = int(input("Enter the number of boxes (default is 8): ") or 8)
-
-
-    # Derived values
-    each_box_width = box_width // n_box 
+    # Sub-boxes values
+    each_box_width = box_width // n_crown 
     middle_box = each_box_width // 2
 
-    queens_color_matrix = np.zeros((n_box, n_box))
+    queens_color_matrix = np.zeros((n_crown, n_crown))  # Initialize queens matrix
 
-    for i in range(n_box):
-        for j in range(n_box):
+    # Obtain matrix of colors
+    for i in range(n_crown):
+        for j in range(n_crown):
             color = screenshot.getpixel((top_left_x + middle_box + j*each_box_width,
                                     top_left_y + middle_box + i*each_box_width))
             queens_color_matrix[i,j] = np.round(0.299*color[0] + 0.587*color[1] + 0.114*color[2], 2)
 
     unique_colors = np.unique(queens_color_matrix)
-    print(len(unique_colors))
-    if len(unique_colors) != n_box:
-        raise Warning(f"Expected {n_box} unique values but found {len(unique_colors)}")
+    if len(unique_colors) != n_crown:
+        raise ValueError(f"Expected {n_crown} unique values but found {len(unique_colors)}")
 
     value_to_index = {val: idx for idx, val in enumerate(unique_colors)}
     indexed_matrix = np.vectorize(lambda x: value_to_index[x])(queens_color_matrix)
 
-    print(indexed_matrix)
+    print(indexed_matrix)   # Show the color matrix in terminal
 
-    matrix_sol, _ = queens_solver(indexed_matrix+1)
+    matrix_sol, _ = queens_solver(indexed_matrix+1) # Solve the game
 
-    for i in range(n_box):
-        for j in range(n_box):
+    # Put crowns in solution
+    for i in range(n_crown):
+        for j in range(n_crown):
             if matrix_sol[i, j] == 1:
-
-                # Convert to int in case of float values
                 pyautogui.click(
                     top_left_x + middle_box + j*each_box_width,
                     top_left_y + middle_box + i*each_box_width, 
